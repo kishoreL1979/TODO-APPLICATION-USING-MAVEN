2bbcf60ab5ea1ea01af64b29a7a2150a0f7e68a5
kishoreL1979 <kit28.24bcb028@gmail.com>
2026-01-08 14:08:31 +0530

    Todo application is created successfully using maven

---
+++ TODO-LIST-APPLICATION-main/src/main/java/com/todo/Main.java
@@ -0,0 +1,10 @@
+package com.todo;
+
+import com.todo.gui.TodoGUI;
+
+public class Main {
+    public static void main(String args[]){
+        TodoGUI app = new TodoGUI();
+        app.setVisible(true);
+    }
+}
---
+++ TODO-LIST-APPLICATION-main/src/main/java/com/todo/model/Todo.java
@@ -0,0 +1,66 @@
+package com.todo.model;
+
+import java.time.LocalDateTime;
+
+public class Todo{
+    private int id;
+    private String title;
+    private String description;
+    private boolean Completed;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+    public Todo() {
+        this.created_at = LocalDateTime.now();
+        this.updated_at = LocalDateTime.now();
+    }
+    public Todo(String title, String description) {
+        this();
+        this.title = title;
+        this.description = description;
+    }
+    public Todo(int id, String title, String description, boolean completed, LocalDateTime created_at,
+            LocalDateTime updated_at) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        Completed = completed;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public boolean isCompleted() {
+        return Completed;
+    }
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setCompleted(boolean completed) {
+        Completed = completed;
+    }
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+}
