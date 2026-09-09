69f00176c2062f074a7cbb0ad7242b6ccd33063a
kishoreL1979 <kit28.24bcb028@gmail.com>
2026-09-09 09:30:48 +0530

    Store SQLite database in user home folder to prevent access denied errors

--- src/main/java/com/todo/util/DatabaseConnection.java
+++ src/main/java/com/todo/util/DatabaseConnection.java
@@ -1,13 +1,22 @@
 package com.todo.util;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:sqlite:todo_db.db";
+    private static String getDatabaseUrl() {
+        String userHome = System.getProperty("user.home");
+        File dbDir = new File(userHome, "TodoApplication");
+        if (!dbDir.exists()) {
+            dbDir.mkdirs();
+        }
+        File dbFile = new File(dbDir, "todo.db");
+        return "jdbc:sqlite:" + dbFile.getAbsolutePath();
+    }
 
     public static Connection getDBConnection() throws SQLException {
-        return DriverManager.getConnection(URL);
+        return DriverManager.getConnection(getDatabaseUrl());
     }
 }
